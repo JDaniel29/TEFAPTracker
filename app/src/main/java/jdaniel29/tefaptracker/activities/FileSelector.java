f629189d4ffc142e686a21d4554b76ca0fe56a2b
Jonathan Daniel <gcagrad19@gmail.com>
2017-10-23 21:11:52 -0400

    Blank File Picker Activity

---
+++ app/src/main/java/jdaniel29/tefaptracker/activities/FileSelector.java
@@ -0,0 +1,14 @@
+package jdaniel29.tefaptracker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import jdaniel29.tefaptracker.R;
+
+public class FileSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_selector);
+    }
+}
